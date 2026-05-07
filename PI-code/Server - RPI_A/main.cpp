6a4628810c31f03ddbd4b3376b8825e3dfc8bdb2
sambam <167071477+SkillfulRheyme4@users.noreply.github.com>
2026-05-07 21:03:58 +0200

    Add main function to initialize the server
    Implement main function to start the server for communication.

---
+++ PI-code/Server - RPI_A/main.cpp
@@ -0,0 +1,11 @@
+#include "server.h"
+
+/**
+ * @brief This starts the Server of the PI that enables the comminucation between the PI and mutiple Wemos
+ * 
+ * @return int 
+ */
+int main() {
+    startServer();
+    return 0;
+}

06c6e3d348c5839fd3b384e7570495234e609cc8
sambam <167071477+SkillfulRheyme4@users.noreply.github.com>
2026-05-14 22:13:32 +0200

    Enhance server communication, RFID support
    Improved message handling from the server, added RFID support

--- Wemos/main.ino
+++ Wemos/main.ino
@@ -2,21 +2,25 @@
 #include "BedSensor.h"
 #include "LedCheck.h"
 #include "DisplayMatrix.h"
+#include "RFID.h"
 
-const char* ssid = "NSELab";
-const char* password = "NSELabWiFi";
+const char* ssid = "happyvibeswifi";
+const char* password = "zoetoefeestschuur";
 
-const char* serverIP = "145.52.127.166";
+const char* serverIP = "192.168.0.217";
 const int serverPort = 5000;
 
-const char DEVICE_ID = 'A';
+char DEVICE_ID = 'A';
 
 WiFiClient client;
-
 BedSensor bed(A0);
 LedCheck led;
+RFID rfid;
 
 void setup() {
+
+  Serial.begin(115200);
+  delay(500);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -24,45 +28,54 @@ void setup() {
   }
 
   client.connect(serverIP, serverPort);
+  client.setTimeout(10);
   client.println(DEVICE_ID);
+  client.print('\n');
   led.begin();
   displayInit();
+  rfid.begin();
+
 }
 
 void loop() {
 
-  // reconnect indien nodig
+  
   if (!client.connected()) {
     client.stop();
 
-    if (client.connect(serverIP, serverPort)) {
-      client.println(DEVICE_ID);
-    }
-
-    delay(1000);
-
-    return;
+    client.connect(serverIP, serverPort);
+    client.println(DEVICE_ID);
+    client.print('\n');
   }
 
-  // DATA VAN SERVER
-  if (client.available()) {
 
-    String msg = client.readStringUntil('\n');
-    msg.trim();
-    if (msg.length() > 0) {
-      if (msg == "1" || msg == "2") {
-        led.handleCommand(msg[0]);
+  static String msg = "";
+
+  while (client.available()) {
+    char c = client.read();
+
+    if (c == '\n') {
+      msg.trim();
+
+      if (msg.length() > 0) {
+        if (msg == "1" || msg == "2") {
+          led.handleCommand(msg[0]);
+        } else {
+          displayShow(msg.c_str());
+        }
       }
-      else {
-        displayShow(msg.c_str());
-      }
+
+      msg = "";
+    } else {
+      msg += c;
     }
   }
+
 
   led.update();
   displayUpdate();
-  bed.update(client);
-  
+  bed.update();
+  rfid.update();
 
   delay(50);
 }

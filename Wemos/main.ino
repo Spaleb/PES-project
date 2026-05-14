b524923722748ffa2d84bb54834a39e2b0a04c5f
sambam <167071477+SkillfulRheyme4@users.noreply.github.com>
2026-05-14 17:58:41 +0200

    Enhance WiFi connection and command handling
    Refactor connection handling and improve command processing.

--- Wemos/main.ino
+++ Wemos/main.ino
@@ -3,10 +3,6 @@
 #include "LedCheck.h"
 #include "DisplayMatrix.h"
 
-/**
- * @brief Sets values for Wemos to connect to the network, server and its own ID.
- * 
- */
 const char* ssid = "NSELab";
 const char* password = "NSELabWiFi";
 
@@ -16,71 +12,57 @@ const int serverPort = 5000;
 const char DEVICE_ID = 'A';
 
 WiFiClient client;
+
 BedSensor bed(A0);
 LedCheck led;
 
-/**
- * @brief Connects to the network and informes the PI that an connection has been made
- * Starts the led and display function
- * 
- */
 void setup() {
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) delay(500);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
 
   client.connect(serverIP, serverPort);
   client.println(DEVICE_ID);
-
   led.begin();
   displayInit();
 }
 
-/**
- * @brief Keeps a connection to the PI
- * 
- * 
- */
 void loop() {
 
+  // reconnect indien nodig
   if (!client.connected()) {
-    client.connect(serverIP, serverPort);
-    client.println(DEVICE_ID);
-    delay(2000);
+    client.stop();
+
+    if (client.connect(serverIP, serverPort)) {
+      client.println(DEVICE_ID);
+    }
+
+    delay(1000);
+
     return;
   }
 
-/**
- * @brief Wemos receives messages from the PI and turns them to cmd text that other functions trigger
- * 
- */
-  while (client.available()) {
-    char cmd = client.read();
+  // DATA VAN SERVER
+  if (client.available()) {
 
-    led.handleCommand(cmd);
-    displayHandleCommand(cmd);
-  }
-
-
-  bool newValue = bed.read();
-  static bool lastValue = false;
-
-  if (newValue != lastValue) {
-    lastValue = newValue;
-
-    client.print("Wemos ");
-    client.print(DEVICE_ID);
-    client.println(" is verbonden.");
-    client.print(";");
-
-    if (newValue) {
-      client.println("BED ON");
-    } else {
-      client.println("BED OFF");
+    String msg = client.readStringUntil('\n');
+    msg.trim();
+    if (msg.length() > 0) {
+      if (msg == "1" || msg == "2") {
+        led.handleCommand(msg[0]);
+      }
+      else {
+        displayShow(msg.c_str());
+      }
     }
   }
 
-
   led.update();
-  displayUpdate(); 
-  delay(200);
+  displayUpdate();
+  bed.update(client);
+  
+
+  delay(50);
 }

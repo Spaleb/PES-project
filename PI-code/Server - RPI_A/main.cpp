26155958441e20453ad3e6d9e49c3dfe7bc73e2d
sambam <167071477+SkillfulRheyme4@users.noreply.github.com>
2026-05-21 12:41:53 +0200

    Refactor main to start Pi and handle Wemos
    Refactor main function to start Pi and handle Wemos in a separate thread.

--- PI-code/Server - RPI_A/main.cpp
+++ PI-code/Server - RPI_A/main.cpp
@@ -1,11 +1,13 @@
 #include "server.h"
+#include <thread>
 
-/**
- * @brief This starts the Server of the PI that enables the comminucation between the PI and mutiple Wemos
- * 
- * @return int 
- */
 int main() {
-    startServer();
+    startPi();
+
+    std::thread wemosThread(initWemos);
+    wemosThread.detach();
+
+    handleWemos();
+
     return 0;
 }

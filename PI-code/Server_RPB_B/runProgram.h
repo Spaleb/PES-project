9439de6aab7529b7d24953c04b377d88f19a3266
sambam <167071477+SkillfulRheyme4@users.noreply.github.com>
2026-06-02 12:32:53 +0200

    Enhance runProgram with message type and handlers
    Added MessageType enum and new methods for message handling.

--- PI-code/Server_RPB_B/runProgram.h
+++ PI-code/Server_RPB_B/runProgram.h
@@ -1,23 +1,34 @@
 #ifndef RUNPROGRAM_H
 #define RUNPROGRAM_H
 
+#include <string>
 #include "TCPServer.h"
 #include "CANInterface.h"
+
+enum class MessageType {
+    BED,
+    ID,
+    UNKNOWN
+};
 
 class runProgram{
 public:
     runProgram();
 
     void run();
+    MessageType getMessageType(const std::string& key);
+    void fallDetection();
 
     ~runProgram();
 
 private:
-    TCPServer tcp;
     CANInterface can;
 
     char buffer[1024];
     void canMessageHandler(const struct can_frame& frame);
+    void handleTcpMessage(const std::string& msg);
+    bool bedPressure;
+    uint16_t readDistance;
 };
 
 #endif

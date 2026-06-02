f0ee0b996477d98dd846458cc2277cb5744252c8
sambam <167071477+SkillfulRheyme4@users.noreply.github.com>
2026-06-02 11:11:31 +0200

    Add LedStrip class header file

---
+++ Wemos/LedStrip.h
@@ -0,0 +1,26 @@
+#ifndef LEDSTRIP_H
+#define LEDSTRIP_H
+
+#include <FastLED.h>
+#include <ESP8266WiFi.h>
+
+// Config
+#define LED_PIN D4
+#define NUM_LEDS 10
+
+class LedStrip {
+public:
+    void begin();
+    void update();
+
+    // Ontvangt de tekst commando's van server
+    void handleCommand(String command, WiFiClient &client);
+
+private:
+    CRGB leds[NUM_LEDS];
+
+    // Blijft bestaan voor lokale aansturing
+    void setLight(uint8_t r, uint8_t g, uint8_t b, uint8_t brightness);
+};
+
+#endif
